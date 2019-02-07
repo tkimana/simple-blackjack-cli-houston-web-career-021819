656127f69ea2b8cda05ab8fd8aedec9b2e3e14f3
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-07 19:57:56 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,13 +1,14 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to Blackjack Table"
 end
 
-def deal_card
-  # code #deal_card here
+def deal_card(number) 
+  puts ""
+  
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total()
+  
 end
 
 def prompt_user
